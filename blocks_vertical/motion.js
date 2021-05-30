1c02998f8decb92d612c897bb7ed786e5e8e3988
ScratchBotManager <84136976+ScratchBotManager@users.noreply.github.com>
2021-05-29 22:13:13 -0400

    Update motion.js

--- blocks_vertical/motion.js
+++ blocks_vertical/motion.js
@@ -582,3 +582,18 @@ Blockly.Blocks['motion_yscroll'] = {
     });
   }
 };
+
+// motion.js
+Blockly.Blocks['motion_move100steps'] = {
+  /**
+   * Block to move 100 steps.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": Blockly.Msg.MOTION_MOVE100STEPS,
+      "category": Blockly.Categories.motion,
+      "extensions": ["colours_motion", "shape_statement"]
+    });
+  }
+};
